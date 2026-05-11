5812627faddacd65d96910850274a2c2b71eee15
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-05-11 09:33:06 +0500

    Add code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import './App.css';
 import CardRecipe from "./components/CardRecipe";
 import React from 'react';
@@ -9,52 +9,52 @@ import React from 'react';
 function App() {
 
 
-const [recipes ,setproducts]=useState([]);
+  const [recipes, setproducts] = useState([]);
 
 
-const gainProducts=(async()=>{
+  const gainProducts = (async () => {
 
-  try{
+    try {
 
-const SetApi=await axios.get(" https://dummyjson.com/recipes")
-setproducts(SetApi.data.recipes)
-console.log(SetApi)
-
-
-  }catch(error){
-console.log("error" ,error)
-  }
+      const SetApi = await axios.get(" https://dummyjson.com/recipes")
+      setproducts(SetApi.data.recipes)
+      console.log(SetApi)
 
 
-
-})
+    } catch (error) {
+      console.log("error", error)
+    }
 
 
 
+  })
 
 
-useEffect(()=>{
-  gainProducts()
-},[])
+
+
+
+  useEffect(() => {
+    gainProducts()
+  }, [])
 
 
 
 
   return (
     <div className="App">
-     <h1 className='heading'>Recipe Card</h1>
-      {recipes.map((product)=>{
+      <h1 className='heading'>Recipe Card</h1>
+      {recipes.map((product) => {
 
-     return(
-      <CardRecipe
-      key={product.id} name={product.name} image={product.image} rating={product.rating}/>
-     )
+        return (
+          <CardRecipe
+            key={product.id} name={product.name} image={product.image} rating={product.rating} />
+        )
 
 
       })}
-    
 
-       
+
+
 
     </div>
   );
--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const CardRecipe = ({name ,image, rating}) => {
+const CardRecipe = ({ name, image, rating }) => {
   return (
     <div className="recipe-container">
 
-    <div className='recipe-style'>
+      <div className='recipe-style'>
         <img src={image} alt="recipe" />
         <h6>{rating}</h6>
         <h1>{name}</h1>
         <button>Add To Cart</button>
-    </div>
+      </div>
     </div>
   )
 }
